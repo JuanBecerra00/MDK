98473d51139df90ff5bd21c5e5bd5dacb96ee65a
Juan Esteban Becerra Tapias <juan.becerra00@misena.edu.co>
2023-01-13 17:39:38 -0500

    implementada vista de recuperar contraseña con el diseño del proyecto

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,11 +1,31 @@
+<style>
+    body{
+            background-color: #000000;
+            background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100%25' height='100%25' viewBox='0 0 1600 800'%3E%3Cg %3E%3Cpolygon fill='%23220000' points='1600 160 0 460 0 350 1600 50'/%3E%3Cpolygon fill='%23440000' points='1600 260 0 560 0 450 1600 150'/%3E%3Cpolygon fill='%23660000' points='1600 360 0 660 0 550 1600 250'/%3E%3Cpolygon fill='%23880000' points='1600 460 0 760 0 650 1600 350'/%3E%3Cpolygon fill='%23A00' points='1600 800 0 800 0 750 1600 450'/%3E%3C/g%3E%3C/svg%3E");
+            background-attachment: fixed;
+            background-size: cover;
+        }
+        input:focus ~ .label{
+                transform:translate(-30px, -60px) scale(0.8);
+                color:black;
+        }
+        input:not(:placeholder-shown) ~ .label{
+            transform:translate(-30px, -60px) scale(0.8);
+            color:black;
+        }
+        .label{
+            transform:translate(0px, -30px);
+            color:black;
+        }
+</style>
 <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
-            <x-jet-authentication-card-logo />
+            <x-jet-application-mark class="block h-20 w-auto" />
         </x-slot>
 
-        <div class="mb-4 text-sm text-gray-600">
-            {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+        <div class="mb-4 text-sm">
+            {{ __('¿Olvido su contraseña? No hay problema. Escriba su direccion de correo para recibir un enlace de reestablecimiento de contraseña.') }}
         </div>
 
         @if (session('status'))
@@ -16,17 +36,18 @@
 
         <x-jet-validation-errors class="mb-4" />
 
-        <form method="POST" action="{{ route('password.email') }}">
+        <form method="POST" action="{{ route('password.email') }}" class="mb-0">
             @csrf
 
             <div class="block">
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                <x-jet-input id="email" placeholder=" " class="block mt-1 w-full border-0 border-b border-black rounded-none
+                    focus:border-black focus:ring-opacity-0 focus:ring-opacity-0 mt-8" type="email" name="email" :value="old('email')" required autofocus />
+                <x-jet-label for="email" value="{{ __('Email') }}" class="label duration-500"/>
             </div>
 
             <div class="flex items-center justify-end mt-4">
                 <x-jet-button>
-                    {{ __('Email Password Reset Link') }}
+                    {{ __('Enviar link') }}
                 </x-jet-button>
             </div>
         </form>
