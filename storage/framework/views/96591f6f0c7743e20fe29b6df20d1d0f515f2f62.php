98b669841233a2ab254a5b45eeaf609ded821019
Juan Esteban Becerra Tapias <juan.becerra00@misena.edu.co>
2023-01-19 19:02:13 -0500

    arreglando el envio de correos img

--- storage/framework/views/96591f6f0c7743e20fe29b6df20d1d0f515f2f62.php
+++ storage/framework/views/96591f6f0c7743e20fe29b6df20d1d0f515f2f62.php
@@ -15,7 +15,7 @@
 <td class="header">
 <a href="<?php echo e($url); ?>" style="display: inline-block;">
 <?php if(trim($slot) === 'Laravel'): ?>
-<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
+<img src="https://github.com/JuanBecerra00/MDK/blob/main/public/assets/textures/logocat.png?raw=true" class="logo" alt="Laravel Logo">
 <?php else: ?>
 <?php echo e($slot); ?>
 
