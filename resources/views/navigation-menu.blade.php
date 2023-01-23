4e73b34825741839528b1fa6383b190b5fdf7ad8
Juan Esteban Becerra Tapias <juan.becerra00@misena.edu.co>
2023-01-23 00:33:16 -0500

    arreglando header

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -178,7 +178,7 @@
 
             <!-- Hamburger -->
             <div class="-mr-2 flex items-center sm:hidden">
-                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition">
+                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-zinc-900 focus:outline-none focus:bg-zinc-700 focus:text-gray-100 transition">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                         <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                         <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
@@ -191,9 +191,37 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-jet-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
+            <x-jet-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')" class="text-white">
                 {{ __('Dashboard') }}
             </x-jet-responsive-nav-link>
+                    
+            @if(Auth::user()->job!='M')
+                    <x-jet-responsive-nav-link href="{{ route('users.index') }}" :active="request()->routeIs('users.index')" class="text-white">
+                        {{ __('Usuarios') }}
+                    </x-jet-responsive-nav-link>
+                    @endif
+                    <x-jet-responsive-nav-link href="{{ route('customers.index') }}" :active="request()->routeIs('customers.index')" class="text-white">
+                        {{ __('Clientes') }}
+                    </x-jet-responsive-nav-link>
+                    <x-jet-responsive-nav-link href="{{ route('vehicles.index') }}" :active="request()->routeIs('vehicles.index')" class="text-white">
+                        {{ __('Vehiculos') }}
+                    </x-jet-responsive-nav-link>
+            @if(Auth::user()->job!='M')
+                    <x-jet-responsive-nav-link href="{{ route('providers.index') }}" :active="request()->routeIs('providers.index')" class="text-white">
+                        {{ __('Proveedores') }}
+                    </x-jet-responsive-nav-link>
+                    @endif
+                    <x-jet-responsive-nav-link href="{{ route('products.index') }}" :active="request()->routeIs('products.index')" class="text-white">
+                        {{ __('Inventario') }}
+                    </x-jet-responsive-nav-link>
+                    <x-jet-responsive-nav-link href="{{ route('reporting.index') }}" :active="request()->routeIs('reporting.index')" class="text-white">
+                        {{ __('Reporte') }}
+                    </x-jet-responsive-nav-link>
+            @if(Auth::user()->job!='M')
+                    <x-jet-responsive-nav-link href="{{ route('billing.index') }}" :active="request()->routeIs('billing.index')" class="text-white">
+                        {{ __('Facturación') }}
+                    </x-jet-responsive-nav-link>
+                    @endif
         </div>
 
         <!-- Responsive Settings Options -->
@@ -206,15 +234,15 @@
                 @endif
 
                 <div>
-                    <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
-                    <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
+                    <div class="font-medium text-base text-white">{{ Auth::user()->name }}</div>
+                    <div class="font-medium text-sm text-white">{{ Auth::user()->email }}</div>
                 </div>
             </div>
 
             <div class="mt-3 space-y-1">
                 <!-- Account Management -->
                 <x-jet-responsive-nav-link href="{{ route('profile.show') }}" :active="request()->routeIs('profile.show')">
-                    {{ __('Profile') }}
+                    <p class="text-white">{{ __('Perfil') }}</p>
                 </x-jet-responsive-nav-link>
 
                 @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
@@ -229,7 +257,7 @@
 
                     <x-jet-responsive-nav-link href="{{ route('logout') }}"
                                    @click.prevent="$root.submit();">
-                        {{ __('Log Out') }}
+                        <p class="text-white">{{ __('Cerrar sesión') }}</p>
                     </x-jet-responsive-nav-link>
                 </form>
 
